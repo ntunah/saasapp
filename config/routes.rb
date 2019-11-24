6156fdc0e7578ec8e4ee1c6c9237613a8eb770f2
Ntuthuko Madondo <ntuthukomadondo777@gmail.com>
2019-11-24 11:25:28 +0200

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
  resources :contacts
+ # create custom URLs
+ # get 'contacts-us.php' , to: 'contacts#new'
 end
